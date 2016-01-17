698be8ec09030db0f32969792047adf2a55bf97a
Andres Meidla <andres.meidla@gmail.com>
2016-01-17 19:59:54 +0200

    added index file, test file and fixed package.json

---
+++ test/getTags.js
@@ -0,0 +1,10 @@
+var hound = require("../build/Release/xml-hound.node");
+
+var expect = require('chai').expect;
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      expect(hound.hello()).to.equal('bla');
+    });
+  });
+});
